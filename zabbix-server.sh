72b56e33a37f5c49fe4c1c045c9321f2902d459c
JordanChenCN <masterchen2012@gmail.com>
2016-11-16 13:56:48 +0800

    Update zabbix-server.sh
    增加了一些交互，修正了一些错误。

--- zabbix-server.sh
+++ zabbix-server.sh
@@ -1,11 +1,23 @@
-#/bin/bash
+#!/bin/bash
 
 #系统要求：mininal centos6.5
 echo "a quck script to install zabbix-server on centos6.5"
 
+read -p "zabbix_version:(default:3.2.1)" zabbix_version
+if [[ -z $zabbix_version ]];then
 zabbix_version=3.2.1
-read -p "zabbix_dir:"  zabbix_dir   #需要交互输入zabbix的目录
-#zabbix_dir=$1                      #不需要交互输入zabbix的目录
+fi
+
+read -p "nginx_version:(default:1.10.2)" nginx_version
+if [[ -z $nginx_version ]];then
+nginx_version=1.10.2
+fi
+
+read -p "php_version:(default:5.6.28)" php_version
+if [[ -z $php_version ]];then
+php_version=5.6.28
+fi
+
 present_dir=`pwd`
 hostname=`hostname`
 hostip=`ip a|grep inet |egrep -v "inet6|127.0.0.1"|cut -d" " -f6|cut -d/ -f1`
@@ -15,56 +27,62 @@ echo "system information:"
 echo "hostname:$hostname"
 echo "ip:$hostip"
 echo "system-version:`cat /etc/centos-release`"
+echo "zabbix_version:$zabbix_version"
+echo "nginx_version:$nginx_version"
+echo "php_version:$php_version"
 
+read -p "are you sure ?(yes or no)" check
+if [[ $check != yes ]];then
+exit 0
+fi
 
 #关闭iptables和selinux
-
-if [ $release = 6 ];then
 setenforce 0
 sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
-
 service iptables stop
 chkconfig iptables off
 
-
-
-
 #安装mysql、php、nginx
-yum install wget epel-release ntpdate mysql-server mysql-devel -y
+yum install wget epel-release ntpdate mysql-server mysql-devel vixie-cron -y
 
 #同步时间
-echo "* */1 * * * /usr/sbin/ntpdate 192.168.83.83 >/dev/null" >> /var/spool/cron/root   
+echo "* */1 * * * /usr/sbin/ntpdate 133.100.11.8 >/dev/null" >> /var/spool/cron/root   
 
-#配置zabbix数据库
+#配置zabbix数据库,默认版本yum源的5.1.73,之后看情况改进
 echo -e "innodb_file_per_table = ON\nskip_name_resolve = ON" >> /etc/my.cnf
 service mysqld start
-mysqladmin  -uroot password "root"
-echo "create database zabbix default charset utf8;" | mysql -uroot -proot
-echo "grant all privileges on zabbix.* to zabbix@'localhost' identified by 'zabbix';" | mysql -uroot -proot
-echo "flush privileges;" | mysql -uroot -proot
 
+read -p "mysql root password:(default:root)" mysql_root_passwd
+if [[ -z $mysql_root_passwd ]];then
+mysql_root_passwd=root
+fi
+
+mysqladmin  -uroot password "$mysql_root_passwd"
+echo "create database zabbix default charset utf8;" | mysql -uroot -p$mysql_root_passwd
+echo "grant all privileges on zabbix.* to zabbix@'localhost' identified by 'zabbix';" | mysql -uroot -p$mysql_root_passwd
+echo "flush privileges;" | mysql -uroot -p$mysql_root_passwd
 
 #安装nginx
 cd $present_dir
-wget http://nginx.org/download/nginx-1.10.2.tar.gz
+wget http://nginx.org/download/nginx-$nginx_version.tar.gz
 yum install pcre* openssl* -y
-tar -xvf nginx-1.10.2.tar.gz
-cd nginx-1.10.2
+tar -xvf nginx-$nginx_version.tar.gz
+cd nginx-$nginx_version
 ./configure --prefix=/usr/local/nginx --with-http_ssl_module --with-http_stub_status_module --with-pcre
 make && make install
 
 #安装php
 cd $present_dir
-wget http://cn2.php.net/distributions/php-5.6.28.tar.gz
+wget http://cn2.php.net/distributions/php-$php_version.tar.gz
 yum install gcc make gd-devel libjpeg-devel libpng-devel libxml2-devel bzip2-devel libcurl-devel -y
-tar -xvf cd php-5.6.28.tar.gz
-cd php-5.6.2
+tar -xvf cd php-$php_version.tar.gz
+cd php-$php_version
 ./configure --prefix=/usr/local/php --with-config-file-path=/usr/local/php/etc --with-bz2 --with-curl --enable-ftp --enable-sockets --disable-ipv6 --with-gd --with-jpeg-dir=/usr/local --with-png-dir=/usr/local --with-freetype-dir=/usr/local --enable-gd-native-ttf --with-iconv-dir=/usr/local --enable-mbstring --enable-calendar --with-gettext --with-libxml-dir=/usr/local --with-zlib --with-pdo-mysql=mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --enable-dom --enable-xml --enable-fpm --with-libdir=lib64 --enable-bcmath
 make && make install
 
 #配置php
 cd $present_dir
-cp php-5.6.28/php.ini-production /usr/local/php/etc/php.ini
+cp php-$php_version/php.ini-production /usr/local/php/etc/php.ini
 sed -i "s/max_execution_time = 30/max_execution_time = 300/g" /usr/local/php/etc/php.ini
 sed -i "s/post_max_size = 8M/post_max_size = 16M/g" /usr/local/php/etc/php.ini
 sed -i "s/max_input_time = 60/max_input_time = 300/g" /usr/local/php/etc/php.ini
@@ -74,10 +92,10 @@ cp /usr/local/php/etc/php-fpm.conf.default /usr/local/php/etc/php-fpm.conf
 
 #编译安装zabbix服务端和客户端
 cd $present_dir
-wget http://nchc.dl.sourceforge.net/project/zabbix/ZABBIX%20Latest%20Stable/3.2.1/zabbix-3.2.1.tar.gz
+wget http://nchc.dl.sourceforge.net/project/zabbix/ZABBIX%20Latest%20Stable/$zabbix_version/zabbix-$zabbix_version.tar.gz
 yum install net-snmp-devel -y
-tar -xvf zabbix-3.2.1.tar.gz
-cd zabbix-3.2.1
+tar -xvf zabbix-$zabbix_version.tar.gz
+cd zabbix-$zabbix_version
 ./configure --prefix=/usr/local/zabbix --with-net-snmp --with-libcurl --enable-server --enable-agent --with-libxml2 --enable-agent --with-mysql
 make && make install
 
@@ -86,9 +104,9 @@ groupadd zabbix
 useradd -g zabbix -m zabbix
 
 #配置zabbix数据库
-/usr/bin/mysql -uroot -proot zabbix < database/mysql/schema.sql
-/usr/bin/mysql -uroot -proot zabbix < database/mysql/images.sql 
-/usr/bin/mysql -uroot -proot zabbix < database/mysql/data.sql
+/usr/bin/mysql -uroot -p$mysql_root_passwd zabbix < database/mysql/schema.sql
+/usr/bin/mysql -uroot -p$mysql_root_passwd zabbix < database/mysql/images.sql 
+/usr/bin/mysql -uroot -p$mysql_root_passwd zabbix < database/mysql/data.sql
 
 #配置zabbix启动项
 cp misc/init.d/tru64/zabbix_* /etc/init.d/
@@ -99,7 +117,7 @@ sed -i "s/# DBPassword=/DBPassword=zabbix/g" /usr/local/zabbix/etc/zabbix_server
 
 #配置并启动nginx、php、zabbix
 mkdir -p /usr/local/zabbix/web
-cp -af $present_dir/zabbix-3.2.1/frontends/php/* /usr/local/zabbix/web
+cp -af $present_dir/zabbix-$zabbix_version/frontends/php/* /usr/local/zabbix/web
 cat > /usr/local/zabbix/web/conf/zabbix.conf.php <<EOF
 <?php
 // Zabbix GUI configuration file.
@@ -128,6 +146,8 @@ cat > /usr/local/nginx/conf/nginx.conf <<EOF
 worker_processes  1;
 events {
     worker_connections  1024;
+        ｝
+    http {
     include       mime.types;
     default_type  application/octet-stream;
     sendfile        on;
@@ -160,13 +180,10 @@ events {
 }
 EOF
 
-
+#启动各进程
 /usr/local/nginx/sbin/nginx
 /usr/local/php/sbin/php-fpm
 service zabbix_server start
 service zabbix_agentd start
 
-echo "请打开浏览器输入http://$ip来进行下一步安装，用户名为admin，密码为zabbix"
-
-
-
+echo "请打开浏览器输入http://$hostip来进行下一步配置，登录用户名为admin，密码为zabbix"
